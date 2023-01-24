95a62f9def9bcac883d612a02c9619d96e22f3b4
johsamy <123368919+johsamy@users.noreply.github.com>
2023-01-24 10:21:59 +0100

    Components

---
+++ ButtonIcon.vue
@@ -0,0 +1,8 @@
+<template>
+    <v-btn color="#90cea1" class="ma-2 black--text">
+        <v-icon>
+            <!-- Specific material design icon value on slot -->
+            mdi-<slot/>
+        </v-icon>
+    </v-btn>
+</template>
---
+++ ButtonText.vue
@@ -0,0 +1,5 @@
+<template>
+    <v-btn color="#90cea1" class="ma-2 black--text">
+        <slot/>
+    </v-btn>
+</template>
