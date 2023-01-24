d5e30d1c10188b76afc62fad0346d1ca611dbbd6
johsamy <123368919+johsamy@users.noreply.github.com>
2023-01-24 10:17:20 +0100

    Layouts

---
+++ default.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app dark>
+    <v-app-bar fixed app>
+      <NuxtLink class="button" :to="{ name: 'index' }">
+        <v-img
+          :src="require(`~/assets/images/blue_short-tmdb.svg`)"
+          max-height="125"
+          max-width="230"
+          class="ml-6"
+        ></v-img>
+      </NuxtLink>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt keep-alive/>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data() {
+    return {
+    }
+  },
+}
+</script>
---
+++ error.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-app dark>
+    <div v-if="error.statusCode === 404">
+      <v-row
+        class="my-8"
+        justify="center"
+        no-gutters
+      >
+        <v-col lg="2">
+            <v-img
+              :src="require(`~/assets/images/404-error.svg`)"
+              class="ma-8"
+            ></v-img>
+          <ErrorAlert>{{ pageNotFound }}</ErrorAlert>
+        </v-col>
+      </v-row>
+    </div>
+    <div v-else>
+      <ErrorAlert>{{ otherError}}</ErrorAlert>
+    </div>
+    <v-row
+      class="my-8"
+      justify="center"
+      no-gutters
+    >
+      <NuxtLink to="/">
+        <ButtonText>
+          Accueil
+        </ButtonText>
+      </NuxtLink>
+    </v-row>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'EmptyLayout',
+  layout: 'empty',
+  props: {
+    error: {
+      type: Object,
+      default: null,
+    },
+  },
+  data() {
+    return {
+      pageNotFound: '404 Page introuvable',
+      otherError: 'Veuillez rafraichir la page ou réessayer plus tard.',
+    }
+  },
+  head() {
+    const title =
+      this.error.statusCode === 404 ? this.pageNotFound : "Erreur"
+    return {
+      title,
+    }
+  },
+}
+
+</script>
+
+<style lang="scss" scoped>
+</style>
